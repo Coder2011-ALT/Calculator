6cbff5ee7d25a172613076e14396aba427ff87f1
Gyaneesh Satyawali <70893994+Coder2011-ALT@users.noreply.github.com>
2021-03-08 21:55:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,9 @@
+﻿function calculate() {
+	var area = Number(
+		document.getElementById("length").value *
+			document.getElementById("length").value
+	);
+	var perimeter = Number(document.getElementById("length").value * 4);
+	document.getElementById("Perimeter").innerHTML = perimeter;
+	document.getElementById("Area").innerHTML = area;
+}
